1efc235b9a6f2fe8e4ebeb90e322401b383a7d92
Igor Chizh <igorchizh@MacBook-Pro-Igor.local>
2023-05-31 20:57:39 +0300

    Test.

--- homework.py
+++ homework.py
@@ -1,18 +1,15 @@
 import os
 from time import time
 from time import sleep
-
 import requests
 import logging
-
+from typing import Dict, Union
 from dotenv import load_dotenv
-
 import telegram
 import telegram.ext
 
 
 load_dotenv()
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -33,7 +30,6 @@ console_handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.addHandler(console_handler)
 
-
 PRACTICUM_TOKEN = os.getenv('YP_API')
 TELEGRAM_TOKEN = os.getenv('TG_API')
 TELEGRAM_CHAT_ID = os.getenv('chatid')
@@ -41,7 +37,6 @@ TELEGRAM_CHAT_ID = os.getenv('chatid')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -73,7 +68,7 @@ def send_message(bot, message):
         logger.error(f"Сбой при отправке сообщения: {error}")
 
 
-def get_api_answer(timestamp: int):
+def get_api_answer(timestamp: int) -> dict:
     """Запрос к эндпоинту."""
     params = {'from_date': timestamp}
     try:
@@ -81,32 +76,66 @@ def get_api_answer(timestamp: int):
         homework_statuses = requests.get(ENDPOINT,
                                          headers=HEADERS,
                                          params=params)
+        # Проверяем, был ли успешный запрос к API.
+        if not 200 <= homework_statuses.status_code < 300:
+            error_message = (f'Запрос к API завершился с ошибкой '
+                             f'{homework_statuses.status_code}')
+            logger.error(error_message)
+            return {}
+
         # Проверяем, есть ли ключ 'homeworks' в ответе.
         if 'homeworks' not in homework_statuses.json():
-            logger.debug('Не найден ключ "homeworks" в ответе.')
-            return None
+            error_message = 'В ответе API отсутствует ключ "homeworks"'
+            logger.error(error_message)
+            return {}
+
         # Получаем список только с домашками.
         response = homework_statuses.json()['homeworks'][0]
+
         return response
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Сбой при запросе к эндпоинту. {e}")
+
+    except requests.exceptions.RequestException as error:
+        logger.error(f"Ошибка запроса к API: {error}")
+        return {}
+
     except Exception as error:
-        logger.error(f"Другая ошибка к эндпоинту: {error}")
+        logger.error(f"Ошибка получения ответа: {error}")
+        return {}
 
 
 def check_response(response: dict):
     """Проверка ответа API на соответствие документации."""
-    if not isinstance(response, dict):
-        logger.debug('Ответ не является словарем')
+    try:
+        if not isinstance(response, dict):
+            error_message = 'Некорректный тип данных в ответе API'
+            logger.error(error_message)
+            raise TypeError(error_message)
+
+        if 'status' not in response:
+            error_message = 'В ответе API отсутствует поле "status"'
+            logger.error(error_message)
+            raise KeyError(error_message)
+
+        if 'homework_name' not in response:
+            error_message = 'В ответе API отсутствует поле "homework_name"'
+            logger.error(error_message)
+            raise KeyError(error_message)
+
+    except Exception as error:
+        logger.error(f"Ошибка проверки ответа: {error}")
 
 
-def parse_status(homework):
+def parse_status(homework: dict) -> str:
     """Извлекаем статус домашней работы."""
     status = homework['status']
+
     if status not in HOMEWORK_VERDICTS:
-        logging.error(f'Неизвестный статус: {status}')
+        error_message = f'Неизвестный статус: {status}'
+        logger.error(error_message)
+        return error_message
+
     verdict = HOMEWORK_VERDICTS[status]
-    homework_name = homework['lesson_name']
+    homework_name = homework['homework_name']
     message = f'Изменился статус проверки работы "{homework_name}". {verdict}'
     return message
 
@@ -128,11 +157,13 @@ def main():
                 send_message(bot, message)
                 timestamp = homework['date_updated']
 
+            time.sleep(RETRY_PERIOD)
+
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logging.exception(message)
             send_message(bot, message)
-            sleep(RETRY_PERIOD)
+            time.sleep(RETRY_PERIOD)
             continue
 
 
